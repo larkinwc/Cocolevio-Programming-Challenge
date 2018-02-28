7888df27787bb2c04952a4262244512e6b5b794d
Larkin Williams-Capone <larkinwc@gmail.com>
2018-02-28 16:58:29 -0600

    Updated Supply.py
    Completed compute data function

--- Supply.py
+++ Supply.py
@@ -5,20 +5,39 @@ def loadData(filename):
     for line in file_in:
         #added rstrip to get rid of newline character
         data.append(line.rstrip().split(','))
+
+    #convert our string values to int, besides first row
+    for i in range(1, len(data)):    
+        data[i] = list(map(int, data[i]))
+
     return data
 
 #use this to determine who to sell to
-def computeData():
+def computeData(supply):
     data = loadData("input.csv")
+    results = []
+    #make multidemensional list
+    results.append([])
+    results.append([])
+    results.append([])
 
-    #compute results
-    results = data
-    
+    while supply > 0:
+        #if we don't have enough merchants
+        if(len(data[2]) == 0):
+            return False
+        
+        highPrice = data[2].index(max(data[2]))
+        supply -= int(data[1][highPrice])
+        results[0].append(data[0].pop(highPrice))
+        results[1].append(data[1].pop(highPrice))
+        results[2].append(data[2].pop(highPrice))
+        
     return results
 
 def main():
     
-    result = computeData()
+    supply = int(input("Please input supply quantity: "))
+
     print(result)
 
 main()
