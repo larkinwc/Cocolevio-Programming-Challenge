0232558cf7e764bd32abf88d6615b67a20dc06a1
Larkin Williams-Capone <larkinwc@gmail.com>
2018-02-28 16:34:35 -0600

    Updated supply.py
    FInished loadData function, had to add rstrip to get rid of the newline character that was coming throug. Could be because I created the CSV manually.

--- Supply.py
+++ Supply.py
@@ -1,8 +1,10 @@
 #use this to load the input file
 def loadData(filename):
-
-    #loadfile
-
+    file_in = open(filename, 'r')
+    data = []
+    for line in file_in:
+        #added rstrip to get rid of newline character
+        data.append(line.rstrip().split(','))
     return data
 
 #use this to determine who to sell to
@@ -10,12 +12,13 @@ def computeData():
     data = loadData("input.csv")
 
     #compute results
-
+    results = data
+    
     return results
 
 def main():
     
     result = computeData()
-
+    print(result)
 
 main()
