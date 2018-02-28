b7f0f86f531ffabb9f53b2d6a98b43b59bd9c3a3
Larkin Williams-Capone <larkinwc@gmail.com>
2018-02-28 16:22:35 -0600

    Started work on python implementation of the solution
    Includes creation of function and dataflow

---
+++ Supply.py
@@ -0,0 +1,21 @@
+#use this to load the input file
+def loadData(filename):
+
+    #loadfile
+
+    return data
+
+#use this to determine who to sell to
+def computeData():
+    data = loadData("input.csv")
+
+    #compute results
+
+    return results
+
+def main():
+    
+    result = computeData()
+
+
+main()
